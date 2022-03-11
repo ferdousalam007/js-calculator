90c0aa1c4e763c226e2c8e7c4e1d6164042d660f
ferdousalam007 <iamferdousalam007@gmail.com>
2022-03-11 12:58:35 +0600

    html css complete js file select all id & class

--- app.js
+++ app.js
@@ -9,23 +9,64 @@ function getClass(classname) {
 
 // get Id 
 const allClear = selectId('all-clear');
-const deleteOne=selectId('delete');
-const equals=selectId('equals');
+const deleteOne = selectId('delete');
+const equals = selectId('equals');
 
 // get class
-const previousOperant = getClass('.previous_operant');
-const currentOperant = getClass('.current_operant');
+const previousOperantElement = document.querySelector('.previous_operant');
+const currentOperantElement = document.querySelector(".current_operant");
 const operator = getClass('.operator');
 const number = getClass('.number');
 
-//common display on UI function
-function displayUI(){
+//global varible declare
+let previousOperant = '';
+let currentOperant = '';
+let operatorchange = '';
+let newoper="";
 
+//01 func value change operator for global variable declare
+function valueChange(value) {
+    currentOperant += value;
+}
+
+//02 func value change operator for global variable declare
+function operatorChange(operator) { 
+    if (currentOperant === "") return;
+    if(!previousOperant){
+        
+    }
+    previousOperant = currentOperant;
+    newoper = operator;
+    
+    operatorchange = previousOperant;
+    currentOperant = '';
+  
+    
+}
+
+
+//func common display on UI function
+function displayUI() {
+    currentOperantElement.innerHTML = currentOperant;
+    previousOperantElement.innerHTML = ` ${operatorchange} ${newoper}`;
 }
 
 //01 number button event
-number.forEach(numBtn=>{
-numBtn.addEventListener('click',function(){
-    console.log(numBtn.textContent);
+number.forEach(numBtn => {
+    numBtn.addEventListener('click', function () {
+        //check for reapeat num value "."
+        if (numBtn.textContent === "." & currentOperant.includes(".")) return;
+        //
+        valueChange(numBtn.textContent);
+        displayUI();
+       
+        
+    })
 })
+//02 operator button event
+operator.forEach(operatorbtn => {
+    operatorbtn.addEventListener('click', function () {
+        operatorChange(operatorbtn.textContent);
+        displayUI();
+    })
 })
